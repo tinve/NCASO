3c8edfb19f0f24a073f643f58e40642828040327
Galina <malov-galina@yandex.ru>
2015-05-25 16:26:17 -0700

    Test for magnetization of 1D, 1.5D, 2D lattices implemented.

--- tests/test_lattice.py
+++ tests/test_lattice.py
@@ -23,7 +23,6 @@ class TestLattice(TestCase):
             sum_n_bruteforce = sum(lattice.state[x] for x in lattice.neighbors[i])
             self.assertEqual(sum_n_bruteforce, sum_n)
 
-
     def test_sum_neighbors_1_5D(self):
         n = 50
         m = 2
@@ -37,7 +36,6 @@ class TestLattice(TestCase):
             sum_n_bruteforce = sum(lattice.state[x] for x in lattice.neighbors[i])
             self.assertEqual(sum_n_bruteforce, sum_n)
 
-
     def test_sum_neighbors_2D(self):
         n = 10
         m = 10
@@ -50,7 +48,6 @@ class TestLattice(TestCase):
             sum_n = lattice.sum_neighbors(i)
             sum_n_bruteforce = sum(lattice.state[x] for x in lattice.neighbors[i])
             self.assertEqual(sum_n_bruteforce, sum_n)
-
 
     def test_energy_1D(self):
         n = 100
@@ -94,6 +91,41 @@ class TestLattice(TestCase):
             energy_bruteforce += J * s * sum(lattice.state[n] for n in lattice.neighbors[i])
         self.assertEqual(energy_bruteforce, energy)
 
+    def test_magnetization_1D(self):
+        n = 100
+        m = 1
+        D = 1
+        J = 1
+        T = 1
+        lattice = Lattice_class.Lattice(n, m, D, J, T)
+
+        magnetization = lattice.magnetization()
+        magnetization_bruteforce = sum(lattice.state)
+        self.assertEqual(magnetization_bruteforce, magnetization)
+
+    def test_magnetization_1_5D(self):
+        n = 50
+        m = 2
+        D = 1.5
+        J = 1
+        T = 1
+        lattice = Lattice_class.Lattice(n, m, D, J, T)
+
+        magnetization = lattice.magnetization()
+        magnetization_bruteforce = sum(lattice.state)
+        self.assertEqual(magnetization_bruteforce, magnetization)
+
+    def test_magnetization_2D(self):
+        n = 10
+        m = 10
+        D = 2
+        J = 1
+        T = 1
+        lattice = Lattice_class.Lattice(n, m, D, J, T)
+
+        magnetization = lattice.magnetization()
+        magnetization_bruteforce = sum(lattice.state)
+        self.assertEqual(magnetization_bruteforce, magnetization)
 
 # def test_deltaM(self):
 #   self.fail()
